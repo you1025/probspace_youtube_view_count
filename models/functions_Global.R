f203c8f2cdfb6353bac42086a50def37fff38a75
you shimajiro <shimajiro@shimajiro.local>
2020-06-28 02:10:44 +0900

    生成特徴量の追加機能

--- models/functions_Global.R
+++ models/functions_Global.R
@@ -26,6 +26,45 @@ load_train_data <- function(path) {
   )
 }
 
+add_extra_features_train <- function(train_data) {
+
+  train_data %>%
+
+    # Tag Point
+    dplyr::left_join(
+      readr::read_csv("models/Ensemble/Stacking/tag_points/output/20200627T061040/tag_points_train_20200627T061040.csv"),
+      by = "id"
+    ) %>%
+
+    # Recent Y
+    dplyr::left_join(
+      readr::read_csv("models/Ensemble/Stacking/use_y_histories/output/20200627T025207_with_impute/avg_y_from_recents_train_20200627T025207.csv"),
+      by = "id"
+    ) %>%
+
+    # Low y Flag
+    dplyr::left_join(
+      readr::read_csv("models/Ensemble/Stacking/low_y_flag/output/20200627T230808_1000/low_y_flags_train_20200627T230808.csv") %>%
+        dplyr::rename(flg_low_y_1000 = predicted),
+      by = "id"
+    ) %>%
+    dplyr::left_join(
+      readr::read_csv("models/Ensemble/Stacking/low_y_flag/output/20200627T235238_5000/low_y_flags_train_20200627T235238.csv") %>%
+        dplyr::rename(flg_low_y_5000 = predicted),
+      by = "id"
+    ) %>%
+    dplyr::left_join(
+      readr::read_csv("models/Ensemble/Stacking/low_y_flag/output/20200628T002124_10000/low_y_flags_train_20200628T002124.csv") %>%
+        dplyr::rename(flg_low_y_10000 = predicted),
+      by = "id"
+    ) %>%
+    dplyr::left_join(
+      readr::read_csv("models/Ensemble/Stacking/low_y_flag/output/20200628T005739_30000/low_y_flags_train_20200628T005739.csv") %>%
+        dplyr::rename(flg_low_y_30000 = predicted),
+      by = "id"
+    )
+}
+
 load_test_data <- function(path) {
   readr::read_csv(
     file = path,
@@ -48,6 +87,45 @@ load_test_data <- function(path) {
       description = col_character()
     )
   )
+}
+
+add_extra_features_test <- function(test_data) {
+
+  test_data %>%
+
+    # Tag Point
+    dplyr::left_join(
+      readr::read_csv("models/Ensemble/Stacking/tag_points/output/20200627T061040/tag_points_test_20200627T061040.csv"),
+      by = "id"
+    ) %>%
+
+    # Recent Y
+    dplyr::left_join(
+      readr::read_csv("models/Ensemble/Stacking/use_y_histories/output/20200627T025207_with_impute/avg_y_from_recents_test_20200627T025207.csv"),
+      by = "id"
+    ) %>%
+
+    # Low y Flag
+    dplyr::left_join(
+      readr::read_csv("models/Ensemble/Stacking/low_y_flag/output/20200627T230808_1000/low_y_flags_test_20200627T230808.csv") %>%
+        dplyr::rename(flg_low_y_1000 = predicted),
+      by = "id"
+    ) %>%
+    dplyr::left_join(
+      readr::read_csv("models/Ensemble/Stacking/low_y_flag/output/20200627T235238_5000/low_y_flags_test_20200627T235238.csv") %>%
+        dplyr::rename(flg_low_y_5000 = predicted),
+      by = "id"
+    ) %>%
+    dplyr::left_join(
+      readr::read_csv("models/Ensemble/Stacking/low_y_flag/output/20200628T002124_10000/low_y_flags_test_20200628T002124.csv") %>%
+        dplyr::rename(flg_low_y_10000 = predicted),
+      by = "id"
+    ) %>%
+    dplyr::left_join(
+      readr::read_csv("models/Ensemble/Stacking/low_y_flag/output/20200628T005739_30000/low_y_flags_test_20200628T005739.csv") %>%
+        dplyr::rename(flg_low_y_30000 = predicted),
+      by = "id"
+    )
 }
 
 
